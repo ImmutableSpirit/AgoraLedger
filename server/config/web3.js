e12e078427ed901b77c402d86e64f0cfd94cb215
immutablespirit <oa227r@gmail.com>
2024-09-28 23:11:44 -0500

    rotating key

--- server/config/web3.js
+++ server/config/web3.js
@@ -1,9 +1,7 @@
 // config/web3.js
 
 const { Web3 } = require('web3');
-const infuraUrl = 'https://mainnet.infura.io/v3/28452fe0d41d45a79526f4b1a340d161';
 
 // Initialize Web3 with Infura
-const web3 = new Web3('https://mainnet.infura.io/v3/28452fe0d41d45a79526f4b1a340d161');
-
+const web3 = new Web3(`https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`);
 module.exports = web3;
--- server/services/blockService.js
+++ server/services/blockService.js
@@ -1,8 +1,7 @@
 // services/blockService.js
 
 const { Web3 } = require('web3');
-const infuraUrl = 'https://mainnet.infura.io/v3/28452fe0d41d45a79526f4b1a340d161';
-const web3 = new Web3('https://mainnet.infura.io/v3/28452fe0d41d45a79526f4b1a340d161');
+const web3 = new Web3(`https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`);
 
 // Helper function to recursively convert BigInt properties to strings
 const convertBigIntToString = (obj) => {
